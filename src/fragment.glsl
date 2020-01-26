f5f1cc0e08045d8a89f61f2d7a7a94096676acd9
max bittker <maxbittker@gmail.com>
2020-01-26 12:03:08 -0800

    scale

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -85,7 +85,7 @@ void main() {
   float m = 100.;
   // pos += noise(vec3(pos * 15., 0.5)) * 0.01;
   float scale = 30. + (noise(vec3(pos * 0.5, t * 0.005)) * 1.5);
-  scale /= 2.0;
+  scale /= 3.0;
   vec3 c = voronoi(scale * pos);
 
   // colorize
